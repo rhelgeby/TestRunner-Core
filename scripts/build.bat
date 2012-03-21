736042035fcfd2edb3d8abb2f458b60fd398e820
Richard Helgeby <richard_helgeby@hotmail.com>
2012-03-21 10:52:20 +0100

    Renamed join script to "build". Added build script for windows.

---
+++ scripts/build.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set VERSION=1.0.0
+set FILE=testrunner-%VERSION%.js
+
+set SRC=..\src\js
+set DEST=..\build
+set DEST_FILE=%DEST%/%FILE%
+
+if not exists "%DEST%" (
+	mkdir %DEST%
+)
+
+
+type %SRC%/assert.js > %DEST_FILE%
+type %SRC%/test_iterator.js >> $DEST_FILE%
+type %SRC%/test_runner.js >> $DEST_FILE
+
+echo Test runner files written to %DEST_FILE%
+
